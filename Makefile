d1cd848a77b36cba33b77a6ef64412d5caf623ab
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2020-05-14 16:37:52 -0700

    Revert to single threaded case, run some experiments

--- Makefile
+++ Makefile
@@ -4,6 +4,16 @@ specs_big := $(wildcard specs/swinv_like_big/l?/*.json)
 swizzle_inventor_tests_small := $(wildcard swizzle-inventor-benchmarks/small_*.rkt)
 swizzle_inventor_tests_big := $(wildcard swizzle-inventor-benchmarks/big_*.rkt)
 
+specs_matmul_large :=\
+specs/swinv_like/l1/trove-crc-2.json\
+specs/swinv_like/l1/trove-crc-3.json\
+specs/swinv_like/l1/trove-crc-4.json\
+specs/swinv_like/l1/trove-rcr-4.json\
+specs/swinv_like/l1/trove-cr_sum-5.json\
+specs/swinv_like/l1/trove-cr_sum-7.json\
+specs/swinv_like_big/l1/trove-crc-5.json\
+specs/swinv_like_big/l1/2d-stencil-5.json
+
 SWIZZLEFLOW_FLAGS ?= -a
 build:
 	cargo build --release
@@ -12,13 +22,20 @@ build-stats:
 
 timings: build ${specs_small}
 	./target/release/swizzleflow ${SWIZZLEFLOW_FLAGS} ${specs_small}
+timings-big: build ${specs_big}
+	./target/release/swizzleflow ${SWIZZLEFLOW_FLAGS} ${specs_big}
 timings-all: build ${specs_small} ${specs_big}
 	./target/release/swizzleflow ${SWIZZLEFLOW_FLAGS} ${specs_big} ${specs_small}
 
 stats: build-stats ${specs_small}
 	./target/release/swizzleflow ${SWIZZLEFLOW_FLAGS} ${specs_small}
+stats-big: build-stats ${specs_big}
+	./target/release/swizzleflow ${SWIZZLEFLOW_FLAGS} ${specs_big}
 stats-all: build-stats ${specs_small} ${specs_big}
 	./target/release/swizzleflow ${SWIZZLEFLOW_FLAGS} ${specs_big} ${specs_small}
+
+matmul-large-timings: build ${specs_matmul_large}
+	./target/release/swizzleflow ${SWIZZLEFLOW_FLAGS} ${specs_matmul_large}
 
 swizzle-inventor: swizzle-inventor-benchmarks/benchmark.sh ${swizzle_inventor_tests_small}
 	./swizzle-inventor-benchmarks/benchmark.sh ${swizzle_inventor_tests_small}
--- src/multiply.rs
+++ src/multiply.rs
@@ -12,7 +12,6 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-use std::cmp::min;
 use std::collections::BTreeMap;
 
 use crate::transition_matrix::{TransitionMatrixOps, TransitionMatrix};
@@ -21,11 +20,6 @@ use crate::misc::{COLLECT_STATS, loghist};
 use itertools::Itertools;
 use itertools::iproduct;
 
-use parking_lot::Mutex;
-use rayon::prelude::*;
-
-
-const TILE_SIZE: usize = 64;
 // Note, because of how these matrices are used
 // the lookup API takes (j, i) not (i, j)
 // Also note that the matrix is really (m x m)x(k x k)
@@ -37,19 +31,15 @@ fn sparsify_mul_no_trans(a: &TransitionMatrix, b: &TransitionMatrix)
         panic!("Invalid shapes for multiply ({}, {}), ({}, {})",
         m, k, k2, n);
     }
-    let m_squared = m.pow(2);
 
-    let c = Mutex::new(TransitionMatrix::empty(a.get_target_shape(),
-                                               b.get_current_shape()));
-    let probes_success = Mutex::new(BTreeMap::new());
-    let probes_failure = Mutex::new(BTreeMap::new());
+    let mut c = TransitionMatrix::empty(a.get_target_shape(),
+                                        b.get_current_shape());
 
-    (0..m_squared/TILE_SIZE).into_par_iter()
-    .for_each_init(
-    || Vec::with_capacity(m_squared / 8),
-    |k_idxs, i_tile| {
-        for i_combined in i_tile*TILE_SIZE..min((i_tile + 1)*TILE_SIZE,m_squared) {
-        let (i1, i2) = (i_combined / m, i_combined % m);
+    let mut probes_success = BTreeMap::new();
+    let mut probes_failure = BTreeMap::new();
+    let mut k_idxs = Vec::with_capacity(m.pow(2) / 8);
+
+    for (i1, i2) in iproduct!(0..m, 0..m) {
         for (kidx1, kidx2) in iproduct!(0..k, 0..k) {
             if a.get_idxs(kidx1, kidx2, i1, i2) {
                 k_idxs.push((kidx1, kidx2))
@@ -59,10 +49,8 @@ fn sparsify_mul_no_trans(a: &TransitionMatrix, b: &TransitionMatrix)
             let mut p = 0;
             for (kidx1, kidx2) in k_idxs.iter().copied() {
                 if b.get_idxs(j1, j2, kidx1, kidx2) {
-                    let mut c = c.lock();
                     c.set_idxs(j1, j2, i1, i2, true);
                     if COLLECT_STATS {
-                        let mut probes_success = probes_success.lock();
                         *probes_success.entry(loghist(p + 1)).or_insert(0) += 1;
                     }
                     break;
@@ -70,20 +58,18 @@ fn sparsify_mul_no_trans(a: &TransitionMatrix, b: &TransitionMatrix)
                 p += 1;
             }
             if COLLECT_STATS && p == k_idxs.len() {
-                let mut probes_failure = probes_failure.lock();
                 *probes_failure.entry(loghist(p)).or_insert(0) += 1;
             }
         }
         k_idxs.clear();
     }
-    });
 
     if COLLECT_STATS {
         println!("mul_stats:: probes_success=[{:?}]; probes_failure=[{:?}];",
-                 probes_success.into_inner().iter().format(", "),
-                 probes_failure.into_inner().iter().format(", "));
+                 probes_success.iter().format(", "),
+                 probes_failure.iter().format(", "));
     }
-    c.into_inner()
+    c
 }
 
 fn sparsify_mul_with_trans(a: &TransitionMatrix, b: &TransitionMatrix)
@@ -95,19 +81,13 @@ fn sparsify_mul_with_trans(a: &TransitionMatrix, b: &TransitionMatrix)
         m, k, k2, n);
     }
 
-    let n_squared = n.pow(2);
+    let mut c = TransitionMatrix::empty(a.get_target_shape(),
+                                        b.get_current_shape());
 
-    let c = Mutex::new(TransitionMatrix::empty(a.get_target_shape(),
-                                               b.get_current_shape()));
-    let probes_success = Mutex::new(BTreeMap::new());
-    let probes_failure = Mutex::new(BTreeMap::new());
-
-    (0..n_squared/TILE_SIZE).into_par_iter()
-    .for_each_init(
-    || Vec::with_capacity(n_squared / 8),
-    |k_idxs, j_tile| {
-    for j_combined in j_tile*TILE_SIZE..min((j_tile+1)*TILE_SIZE, n_squared) {
-        let (j1, j2) = (j_combined / n, j_combined % n);
+    let mut probes_success = BTreeMap::new();
+    let mut probes_failure = BTreeMap::new();
+    let mut k_idxs = Vec::with_capacity(m.pow(2) / 8);
+    for (j1, j2) in iproduct!(0..n, 0..n) {
         for (kidx1, kidx2) in iproduct!(0..k, 0..k) {
             if b.get_idxs(j1, j2, kidx1, kidx2) {
                 k_idxs.push((kidx1, kidx2))
@@ -118,10 +98,8 @@ fn sparsify_mul_with_trans(a: &TransitionMatrix, b: &TransitionMatrix)
             let mut p = 0;
             for (kidx1, kidx2) in k_idxs.iter().copied() {
                 if a.get_idxs(kidx1, kidx2, i1, i2) {
-                    let mut c = c.lock();
                     c.set_idxs(j1, j2, i1, i2, true);
                     if COLLECT_STATS {
-                        let mut probes_success = probes_success.lock();
                         *probes_success.entry(p + 1).or_insert(0) += 1;
                     }
                     break;
@@ -129,28 +107,28 @@ fn sparsify_mul_with_trans(a: &TransitionMatrix, b: &TransitionMatrix)
                 p += 1;
             }
             if COLLECT_STATS && p == k_idxs.len() {
-                let mut probes_failure = probes_failure.lock();
                 *probes_failure.entry(p).or_insert(0) += 1;
             }
         }
         k_idxs.clear();
     }
-    });
 
     if COLLECT_STATS {
         println!("mul_stats:: probes_success=[{:?}]; probes_failure=[{:?}];",
-                 probes_success.into_inner().iter().format(", "),
-                 probes_failure.into_inner().iter().format(", "));
+                 probes_success.iter().format(", "),
+                 probes_failure.iter().format(", "));
     }
-    c.into_inner()
+    c
 }
 
 pub fn sparsifying_mul(a: &TransitionMatrix, b: &TransitionMatrix)
                        -> TransitionMatrix {
     if a.n_ones() <= b.n_ones() {
+        println!("Multiply: No transpose");
         sparsify_mul_no_trans(a, b)
     }
     else {
+        println!("Multiply: transpose");
         sparsify_mul_with_trans(a, b)
     }
 }
